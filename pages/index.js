6f032890187b9ff9636ab3112cacbd78eeaa7b12
zc <zchao@51box.cn>
2022-01-05 11:11:06 +0800

    antd test

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
 import Head from 'next/head'
-
+import { Layout, Rate } from 'antd';
+import 'antd/dist/antd.css';
+const { Header, Footer, Sider, Content } = Layout;
 const Home = () => (
   <div className="container">
     <Head>
       <title>LuoFei</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    <Layout style={{ width: "100%" }}>
+      <Header>
+        <h1 className="title">
+          LuoFei Server
+        </h1>
+      </Header>
+      <Layout>
+        <Content>
+          <p className="description">
+            在这里， 得到自己的成长
+          </p>
+        </Content>
+        <Sider><Rate allowHalf defaultValue={2.5} /></Sider>
+      </Layout>
+      <Footer>
+        <p className="description">
+          开始吧 serverless
+        </p>
+      </Footer>
+    </Layout>
+
 
     <main>
-      <h1 className="title">
-        LuoFei Server
-      </h1>
 
-      <p className="description">
-        在这里， 得到自己的成长
-      </p>
-      <p className="description">
-        开始吧 serverless
-      </p>
+
+
+
     </main>
   </div>
 )
